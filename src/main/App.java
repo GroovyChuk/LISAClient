3ea66ef543aa67e26351ce5356d2182dd51c6177
Kalaman <oemer.kalaman@hotmail.de>
2018-01-15 20:43:33 +0100

    Changed the way of switching between layouts. You're able to switch between scenes instead of replacing a pane

---
+++ src/main/App.java
@@ -0,0 +1,47 @@
+package main;
+
+import MQTT.MQTTClient;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class App extends Application {
+
+    public static MQTTClient mqttClient;
+    public static Stage window;
+    public static Scene loginScene;
+    public static Scene mainScene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+
+        loginScene = new Scene(FXMLLoader.load(getClass().getResource("../layouts/login_screen.fxml")), 800, 400);
+        mainScene = new Scene(FXMLLoader.load(getClass().getResource("../layouts/main_screen.fxml")), 800, 400);
+
+        window.setTitle("LISA Client v0.01");
+        window.setScene(mainScene);
+        window.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                try {
+                    mqttClient.stopSessionThread();
+                    stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        window.show();
+    }
+
+
+    public static void main(String[] args) {
+        mqttClient = new MQTTClient();
+        mqttClient.startSessionThread();
+        launch(args);
+    }
+}

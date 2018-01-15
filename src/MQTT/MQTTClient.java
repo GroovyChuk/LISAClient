56b1be55bd0ade39be9f3786ff17028730b4a7f8
Kalaman <oemer.kalaman@hotmail.de>
2018-01-15 12:50:13 +0100

    Added MQTTClient

---
+++ src/MQTT/MQTTClient.java
@@ -0,0 +1,70 @@
+package MQTT;
+
+import org.fusesource.mqtt.client.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+/**
+ * Created by Kalaman on 09.01.18.
+ */
+public class MQTTClient {
+    private MQTT mqtt;
+    private BlockingConnection connection;
+    private Topic [] topic;
+
+    private ArrayList<MQTTListener> mqttListener;
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 1883;
+    private static final String TOPIC_NAME = "login";
+
+    public MQTTClient() {
+        mqtt = new MQTT();
+        mqttListener = new ArrayList<MQTTListener>();
+        topic = new Topic[] {new Topic(TOPIC_NAME, QoS.EXACTLY_ONCE)};
+
+        try {
+            mqtt.setHost(SERVER_IP, SERVER_PORT);
+            connection = mqtt.blockingConnection();
+            connection.connect();
+            connection.subscribe(topic);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public interface MQTTListener {
+        public void onSessionCodeScanned(String sessionID);
+    }
+
+    public boolean addMQTTListener(MQTTListener listener) {
+        return mqttListener.add(listener);
+    }
+
+    public boolean removeMQTTListener(MQTTListener listener) {
+        return mqttListener.remove(listener);
+    }
+
+    public void startSessionThread () {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Message message = connection.receive();
+                    String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
+                    message.ack();
+
+                    for (MQTTListener listener : mqttListener)
+                        listener.onSessionCodeScanned(payload);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }.start();
+    }
+
+}

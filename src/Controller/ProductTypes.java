da6b4f380f17c0e1b44626c37a4c230bc90d49ad
Kalaman <oemer.kalaman@hotmail.de>
2018-01-16 13:02:14 +0100

    Added new attributes and constants

---
+++ src/Controller/ProductTypes.java
@@ -0,0 +1,11 @@
+package Controller;
+
+/**
+ * Created by Kalaman on 16.01.18.
+ */
+public class ProductTypes {
+    public static final String OBJECT = "Gegenstand";
+    public static final String MEAT = "Fleischprodukt";
+    public static final String VEGAN = "Vegan";
+    public static final String VEGATARIAN = "Vegetarisch";
+}

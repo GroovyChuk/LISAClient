03c76a98c2c417e07af844d3eddb5383aa8d2775
Kalaman <oemer.kalaman@hotmail.de>
2018-01-16 12:58:40 +0100

    Added new labels and a new method to update the UI

--- src/Controller/MainScreen.java
+++ src/Controller/MainScreen.java
@@ -2,11 +2,13 @@ package Controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
@@ -22,6 +24,10 @@ public class MainScreen{
     @FXML
     AnchorPane anchorPane;
     @FXML
+    AnchorPane anchorPaneCurrentProduct;
+    @FXML
+    AnchorPane anchorPaneCurrentProductNutInfo;
+    @FXML
     ImageView imageUser;
     @FXML
     ListView<Product> listviewCart;
@@ -30,21 +36,45 @@ public class MainScreen{
     @FXML
     Pane paneEmptyShoppingCart;
 
+    @FXML
+    Label labelProductName;
+    @FXML
+    Label labelProductPrice;
+    @FXML
+    Label labelProductProducer;
+    @FXML
+    Label labelProductType;
+
+    @FXML
+    Label labelProductCalories;
+    @FXML
+    Label labelProductCarbohydrate;
+    @FXML
+    Label labelProductFat;
+
     private ObservableList<Product> cartList = FXCollections.observableArrayList();
 
 
     @FXML
     private void initialize() throws IOException {
 
-        addProduct(new Product("Snickers",1.5f));
-        addProduct(new Product("Mars",1f));
-        addProduct(new Product("Macadamia",2.3f));
-        addProduct(new Product("Peanuts",0.5f));
+        addProduct(new Product("Snickers","Mars",1.5f, ProductTypes.VEGAN,new NutritionFact(481,60.5f,22.5f)));
+        addProduct(new Product("Mars","Mars",1f,ProductTypes.VEGAN,new NutritionFact(449,70.2f,16.6f)));
+        addProduct(new Product("Macadamia","Bauer Alfred",2.3f,ProductTypes.VEGAN,new NutritionFact(745,4.2f,75.2f)));
+        addProduct(new Product("Peanuts","KA Company",0.5f,ProductTypes.VEGAN,new NutritionFact(615,9.3f,52.9f)));
+        addProduct(new Product("Xbox One","Microsoft",319.99f,ProductTypes.OBJECT,null));
 
         listviewCart.setCellFactory(new Callback<ListView<Product>, ListCell<Product>>() {
             @Override
             public ListCell<Product> call(ListView<Product> param) {
                 return new ProductListCell();
+            }
+        });
+
+        listviewCart.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                updateUI();
             }
         });
 
@@ -63,6 +93,30 @@ public class MainScreen{
     }
 
     public void updateUI() {
+        int selectedIndex = listviewCart.getSelectionModel().getSelectedIndices().get(0);
+        Product selectedProduct = null;
+
+        if (selectedIndex >= 0){
+            selectedProduct = cartList.get(selectedIndex);
+
+            if (selectedProduct.getProductType() == ProductTypes.OBJECT)
+                anchorPaneCurrentProductNutInfo.setVisible(false);
+            else
+            {
+                anchorPaneCurrentProductNutInfo.setVisible(true);
+                labelProductCarbohydrate.setText(selectedProduct.getNutritionFact().getFormatedCarbohydrates());
+                labelProductCalories.setText(selectedProduct.getNutritionFact().getFormatedCalories());
+                labelProductFat.setText(selectedProduct.getNutritionFact().getFormatedTotalFat());
+            }
+
+            if (selectedIndex >= 0) {
+                labelProductName.setText(selectedProduct.getName());
+                labelProductPrice.setText(selectedProduct.getFormatedPrice());
+                labelProductProducer.setText(selectedProduct.getProducer());
+                labelProductType.setText(selectedProduct.getProductType());
+            }
+        }
+
         float productSum= 0;
 
         for (Product product : cartList)
@@ -70,10 +124,11 @@ public class MainScreen{
 
         labelProductSum.setText("Summe: " + productSum + " €");
 
-        if(cartList.size() > 0 )
+        if(cartList.size() > 0)
             paneEmptyShoppingCart.setVisible(false);
         else
             paneEmptyShoppingCart.setVisible(true);
+
     }
 
 }

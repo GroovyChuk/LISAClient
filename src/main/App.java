ab83ac24d52db829aa6788e0471c59dbd1822317
Kalaman <oemer.kalaman@hotmail.de>
2018-01-16 13:07:29 +0100

    Modified tab and spaces

--- src/main/App.java
+++ src/main/App.java
@@ -38,7 +38,6 @@ public class App extends Application {
         window.show();
     }
 
-
     public static void main(String[] args) {
         mqttClient = new MQTTClient();
         mqttClient.startSessionThread();

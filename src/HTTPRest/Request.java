1cee8bd14a6edfe9d2bce705c9cb07627f24d802
GroovyChuk <alasdair333@hotmail.com>
2018-01-22 22:38:48 +0100

    Fetch Json Object from URL

---
+++ src/HTTPRest/Request.java
@@ -0,0 +1,42 @@
+package HTTPRest;
+
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+/**
+ * Created by alasdair on 22.01.18.
+ */
+public class Request {
+
+    public JSONObject getURL(URL url){
+        JSONObject jsonObject = new JSONObject();
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            InputStream inputStream = conn.getInputStream();
+            JSONParser jsonParser = new JSONParser();
+            jsonObject = (JSONObject)jsonParser.parse(
+                    new InputStreamReader(inputStream, "UTF-8"));
+
+
+            conn.disconnect();
+        } catch (Exception e){
+
+        }
+
+        return jsonObject;
+    }
+}

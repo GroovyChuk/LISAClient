fbe6b2f698c79ebc05977d0d715742f89560315a
GroovyChuk <alasdair333@hotmail.com>
2018-01-23 15:36:22 +0100

    Platform.runLater for JavaFX added

---
+++ src/scripts/Read.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import time
+
+continue_reading = True
+
+# Create an object of the class MFRC522
+#MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+# print "Welcome to the MFRC522 data read example"
+# print "Press Ctrl-C to stop."
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    time.sleep(5)
+    continue_reading = False
+
+    print(4388810057817)
+
+continue_reading = True

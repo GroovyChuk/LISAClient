887abea61fbafd7852cfc5750b7df04727f5a073
GroovyChuk <alasdair333@hotmail.com>
2018-02-05 01:42:58 +0100

    Product with same GTIN get added and removed correctly now Scales Thread support LED Scripts

--- src/frame/JProductPanel.java
+++ src/frame/JProductPanel.java
@@ -6,6 +6,7 @@ import HTTPRest.Request;
 import main.App;
 import main.JConstants;
 import main.RFIDReaderThread;
+import main.ScalesReaderThread;
 import org.json.simple.JSONObject;
 import product.NutritionFact;
 import product.Product;
@@ -16,21 +17,22 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class JProductPanel extends JPanel implements ListSelectionListener, RFIDReaderThread.RFIDReader{
+public class JProductPanel extends JPanel implements ListSelectionListener, ScalesReaderThread.ScalesReader, RFIDReaderThread.RFIDReader{
 
     private JList list;
     private DefaultListModel listModel;
-    private Thread readerThread;
+    private Thread readerThread , scalesThread;
     private ArrayList<Product> cartItems = new ArrayList<Product>();
     private float cartSum = 0.00f;
-    private JLabel sumLabel;
+    private JLabel sumLabel , weightLabel, scaleLabel;
     private JProductDetailsPanel jProductDetailsPanel;
-
-    private JButton removeButton;
+    int weight = 0;
+    private JButton purchaseButton;
 
     public JProductPanel(JProductDetailsPanel jProductDetails) {
         this.jProductDetailsPanel = jProductDetails;
@@ -47,25 +49,32 @@ public class JProductPanel extends JPanel implements ListSelectionListener, RFID
         list.setVisibleRowCount(5);
         JScrollPane listScrollPane = new JScrollPane(list);
 
-        removeButton = new JButton("Remove");
-        removeButton.addActionListener(new RemoveListener());
+
 
         //Create a panel that uses BoxLayout.
         JPanel buttonPane = new JPanel();
         sumLabel = new JLabel();
+        weightLabel = new JLabel();
+        scaleLabel = new JLabel();
+        scaleLabel.setText("Bitte stellen Sie den gescannten Artikel auf die Waage.");
+        scaleLabel.setVisible(false);
         updateCartSum();
+        updateScales(0);
+        purchaseButton = new JButton("Kaufen");
+        purchaseButton.addActionListener(new PurchaseListener());
         buttonPane.setLayout(new BorderLayout());
-        buttonPane.add(removeButton,BorderLayout.WEST);
-        buttonPane.add(sumLabel, BorderLayout.EAST);
-        buttonPane.add(Box.createHorizontalStrut(5));
-        buttonPane.add(Box.createHorizontalStrut(5));
+        buttonPane.add(sumLabel, BorderLayout.LINE_END);
+        buttonPane.add(purchaseButton,BorderLayout.NORTH);
+        buttonPane.add(weightLabel, BorderLayout.LINE_START);
 
-        buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        buttonPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
         add(listScrollPane, BorderLayout.CENTER);
         add(buttonPane, BorderLayout.PAGE_END);
+        add(scaleLabel, BorderLayout.PAGE_START);
         readerThread = new RFIDReaderThread(this);
-        readerThread.start();
+        scalesThread = new ScalesReaderThread(this);
     }
 
 
@@ -75,53 +84,50 @@ public class JProductPanel extends JPanel implements ListSelectionListener, RFID
 
             if (list.getSelectedIndex() == -1) {
                 //No selection, disable fire button.
-                jProductDetailsPanel.setProductDetails("Produktdetails","","","");
-                removeButton.setEnabled(false);
+                jProductDetailsPanel.setProductDetails("Produktdetails","","","", "");
 
             } else if (list.getSelectedIndex() != -1){
-                int index = 0;
+                int index = 0, productIndex = 0;
                 Product p;
-                for (int i = 0; i < cartItems.size(); i++) {
-                    if (listModel.get(i).toString().contains(cartItems.get(i).getName()))
-                        index = i;
+                for (int i = 0; i < listModel.size(); i++) {
+                    for (int j = 0; j < cartItems.size(); j++) {
+                        if (listModel.get(i).toString().contains(cartItems.get(j).getName())){
+                            index = i;
+                            productIndex = j;
+                        }
+                    }
+
                 }
                 p = cartItems.get(index);
-                jProductDetailsPanel.setProductDetails(p.getName(),p.getProducer(),p.getProductType(),p.getFormatedPrice());
-
-                //Selection, enable the fire button.
-                removeButton.setEnabled(true);
-            }
-        }
-    }
-
-    class RemoveListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            //This method can be called only if
-            //there's a valid selection
-            //so go ahead and remove whatever's selected.
-            int index = list.getSelectedIndex();
-            listModel.remove(index);
-
-            int size = listModel.getSize();
-
-            if (size == 0) { //Nobody's left, disable firing.
-                removeButton.setEnabled(false);
-
-            } else { //Select an index.
-                if (index == listModel.getSize()) {
-                    //removed item in last position
-                    index--;
+                int occurrence = 0;
+                //find occurrence of same Products
+                for (int i = 0; i < cartItems.size(); i++) {
+                    if (cartItems.get(i).getsGTIN().substring(0,13).equals(cartItems.get(productIndex).getsGTIN().substring(0,13)))
+                        occurrence++;
                 }
-
-                list.setSelectedIndex(index);
-                list.ensureIndexIsVisible(index);
+                jProductDetailsPanel.setProductDetails(p.getName(),p.getProducer(),p.getProductType(),p.getFormatedPrice(),"" + occurrence);
 
             }
         }
     }
+
 
     public void addProduct (Product product) {
-        listModel.insertElementAt(product.getName() + "   " + product.getPrice() + " €",0);
+        int occurrence = 1;
+        //find occurrence of same Products
+        for (int i = 0; i < cartItems.size(); i++) {
+            if (cartItems.get(i).getsGTIN().substring(0,13).equals(product.getsGTIN().substring(0,13)))
+                occurrence++;
+        }
+        if (occurrence == 1)
+            listModel.insertElementAt(product.getName() + "   " + product.getPrice() + " €",0);
+        else {
+            for (int i = 0; i < listModel.size(); i++) {
+                if (listModel.get(i).toString().contains(product.getName()))
+                    listModel.removeElementAt(i);
+            }
+            listModel.insertElementAt(occurrence + "x " + product.getName() + "   " + (product.getPrice() * occurrence) + " €", 0);
+        }
         cartItems.add(product);
         cartSum += product.getPrice();
         updateCartSum();
@@ -129,12 +135,35 @@ public class JProductPanel extends JPanel implements ListSelectionListener, RFID
     }
 
     public void removeProduct (Product product) {
+        int occurrence = 0, removeIndex = -1;
+
+        //find occurrence of same Products
+        for (int i = 0; i < cartItems.size(); i++) {
+            if (cartItems.get(i).getsGTIN().substring(0,13).equals(product.getsGTIN().substring(0,13))) {
+                occurrence++;
+                if (cartItems.get(i).getsGTIN().equals(product.getsGTIN()))
+                    removeIndex = i;
+            }
+                       }
+        if (removeIndex != -1){
+            cartItems.remove(removeIndex);
+            occurrence--;
+        }
+
         for (int i = 0; i < listModel.size(); i++) {
             if (listModel.get(i).toString().contains(product.getName()))
                 listModel.remove(i);
-            if (cartItems.get(i).getsGTIN() == product.getsGTIN())
-                cartItems.remove(i);
+                if (occurrence == 0) {
+                    break;
+                } else if (occurrence == 1) {
+                    listModel.insertElementAt(product.getName() + "   " + product.getPrice() + " €",i);
+
+                } else {
+                    listModel.insertElementAt(occurrence + "x " + product.getName() + "   " + (product.getPrice() * occurrence) + " €", i);
+                }
+
         }
+
         cartSum -= product.getPrice();
         updateCartSum();
         updateUI();
@@ -145,24 +174,32 @@ public class JProductPanel extends JPanel implements ListSelectionListener, RFID
         Request request = new Request();
 
         try {
-            JSONObject jsonObject = request.getURL(new URL("http://" + App.IP + ":5000/products/" + rfidCode));
+            JSONObject jsonObject = request.getURL(new URL("http://" + App.IP + ":5000/products/" + rfidCode.substring(0,13)));
             Product product = new Product(jsonObject.get("Name").toString(), "Rewe",
                     Float.valueOf(jsonObject.get("Preis").toString()),"Getränk" ,
-                    new NutritionFact(0,0.0f,0.0f));
-
+                    new NutritionFact(0,0.0f,0.0f),rfidCode, Integer.parseInt(jsonObject.get("Gewicht").toString()));
             Thread add = new Thread() {
                 @Override
                 public void run() {
                     try {
                         boolean added = false;
                         for (Product cartItem : cartItems) {
-                            if (cartItem.getsGTIN() == product.getsGTIN())
+                            if (cartItem.getsGTIN().equals(product.getsGTIN()))
                                 added = true;
                         }
-                        if (!added) { // insert check for serial number here
+                        if (!added) {
+                            while ((product.getWeight() * 0.7 > weight ) || (product.getWeight() * 1.3 < weight)){
+                                scaleLabel.setVisible(true);
+                            }
+                            scaleLabel.setVisible(false);
                             addProduct(product);
+                            ProcessBuilder pb = new ProcessBuilder("python", "/home/pi/Documents/greenled.py");
+                            Process p = pb.start();
+
                         } else {
                             removeProduct(product);
+                            ProcessBuilder pb = new ProcessBuilder("python", "/home/pi/Documents/redled.py");
+                            Process p = pb.start();
                         }
 
                     }
@@ -180,9 +217,44 @@ public class JProductPanel extends JPanel implements ListSelectionListener, RFID
         }
 
     }
+    public void onScalesScanned(String weight) {
+        System.out.println(weight);
+        this.weight = Integer.parseInt(weight);
+        updateScales(this.weight);
+    }
+
+
+    public void startCartThreads() {
+
+        ProcessBuilder tare = new ProcessBuilder("python", App.SCALES_TARE_SCRIPT_PATH);
+        try {
+            tare.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scalesThread.start();
+        readerThread.start();
+    }
 
     public void updateCartSum(){
         sumLabel.setText("Summe: " + cartSum + " €");
+    }
+
+    public void updateScales(int weight){
+        weightLabel.setText("Gewicht: " + weight + " g");
+    }
+
+    class PurchaseListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            try {
+                URL purchase = new URL("http://" + App.IP + ":5000/session/purchase/" + App.sessionKey + "/" + cartSum);
+                Request.getURL(purchase);
+            } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
     }
 
 }

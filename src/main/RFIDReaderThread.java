aea523d98c6e1f6474ebaec51f15deaa9ee05372
GroovyChuk <alasdair333@hotmail.com>
2018-02-05 01:46:11 +0100

    only Reads GTINS with 16 characters now

--- src/main/RFIDReaderThread.java
+++ src/main/RFIDReaderThread.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,24 +21,24 @@ public class RFIDReaderThread extends Thread{
 
     @Override
     public void run() {
+
         super.run();
         try {
             while (true) {
-                ProcessBuilder pb = new ProcessBuilder("python", App.PYTHON_SCRIPT_PATH);
+                ProcessBuilder pb = new ProcessBuilder("python", App.RFID_SCRIPT_PATH);
+
                 Process p = pb.start();
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
                 String line;
 
                 while ((line = in.readLine()) != null) {
-                    listener.onRFIDScanned(line);
+                    if (line.length() == 16)
+                        listener.onRFIDScanned(line);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    //    /home/alasdair/IdeaProjects/LISAClient/src/scripts/Read.py
-
-
 }

f9808d22ded6e520a1bd11a94a013e24ee3a9bd2
GroovyChuk <alasdair333@hotmail.com>
2018-02-06 01:17:41 +0100

    Weight attribute added

--- src/product/Product.java
+++ src/product/Product.java
@@ -7,14 +7,19 @@ public class Product {
     private String productType;
     private NutritionFact nutritionFact;
     private String sGTIN;
+    private int weight;
 
-    public Product (String name, String producer, float price, String productType, NutritionFact nutritionFact) {
+    public Product (String name, String producer, float price, String productType, NutritionFact nutritionFact, String sGTIN, int weight) {
         this.name = name;
         this.price = price;
         this.nutritionFact = nutritionFact;
         this.producer = producer;
         this.productType = productType;
+        this.weight = weight;
+        this.sGTIN = sGTIN;
     }
+
+    public int getWeight() { return weight; }
 
     public String getName() {
         return name;
